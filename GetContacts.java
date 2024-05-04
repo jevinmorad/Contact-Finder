970c476760a7a91b9684ceca89e7f02464036bf1
jevinmorad <23010101171@darshan.ac.in>
2024-05-04 12:32:48 +0530

    Updated listing of contacts

---
+++ GetContacts.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+class GetContacts {
+    public void getContacts() {
+        HashMap<String, String> map = new HashMap<>();
+
+        ContactSet set = new ContactSet();
+        map = set.getSet();
+        
+        Set<Map.Entry<String, String>> entrySet = map.entrySet();
+        
+        System.out.println(" Name                   Number\n");
+        for (Map.Entry<String, String> entry : entrySet) {
+            System.out.printf(" %-20s %s%n", entry.getValue(), entry.getKey());
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,4 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+class ContactSet{
+    public HashMap<String, String> getSet(){
+        HashMap<String, String> contactMap = new HashMap<String, String>();
+        try {
+            File contact = new File("contact.csv");
+            FileReader fr = new FileReader(contact);
+            BufferedReader br = new BufferedReader(fr);
+            // Creared a HashMap with phoneNumber as a key and name as value
+            String line;
+            // reading character untill file ends
+            while ((line = br.readLine()) != null) {
+                // dividing line into parts and storing it in an array
+                String[] part = line.split(",");
+                // storing phoneNumber and name in the HashMap
+                contactMap.put(part[0], part[1]);
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return contactMap;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -17,8 +42,8 @@ public class Main {
         System.out.println();
         switch (choice) {
             case 1:
-                ListContacts contactList = new ListContacts();
-                contactList.listOfContacts();
+                GetContacts contacts = new GetContacts();
+                contacts.getContacts();
                 break;
             case 0:
                 System.exit(0);
